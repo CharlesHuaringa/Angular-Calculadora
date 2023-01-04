00a0d86dd7fa1beb3713d471a42c234bed9b40f9
Ruth-pm <palominoruth175@gmail.com>
2023-01-04 17:04:37 -0500

    Agregando contenido

---
+++ src/app/calc/content-fecha/content-fecha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-fecha',
+  templateUrl: './content-fecha.component.html',
+  styleUrls: ['./content-fecha.component.css']
+})
+export class ContentFechaComponent {
+
+}
---
+++ src/app/calc/content-operation/content-operation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-operation',
+  templateUrl: './content-operation.component.html',
+  styleUrls: ['./content-operation.component.css']
+})
+export class ContentOperationComponent {
+ 
+}
